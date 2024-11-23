216c87bf56637d5049dda289dd3f4dcbc43602fa
federicacasartelli <127952734+federica0709@users.noreply.github.com>
2024-11-23 09:42:40 +0100

    Assignment 3 completo

--- sketch.js
+++ sketch.js
@@ -15,7 +15,9 @@ function preload() {
 function setup() {
      //dataObj = data.getObject();
      dataObj = data.getRows().map(row => {
-      // Estrai il valore della colonna `length` e converti in numero
+      // Estrai il valore delle colonne name, countries, continent, 
+      // length, outflow e convertili in numero in modo tale da poter 
+      // effettuare in seguito un ordinamento
       return {
         name: row.get("name"),
         countries: row.get("countries"),
@@ -33,43 +35,43 @@ function setup() {
         return !isDuplicate;
     });
 
+   // comando per calcolare il fiume con lunghezza maggiore 
+   // riscalata poi con la funzione map
     let maxRiverLength = Math.max(...dataObj.map(item => item.length));
 
   // Verifica la struttura di `dataObj` dopo la conversione
   console.log("Data after conversion:", dataObj);
-
-  // Recupera i dati sotto forma di oggetto
  
   // Ordina i dati per lunghezza in ordine decrescente
   dataObj.sort((a, b) => b.length - a.length);
+  
   // Verifica l’ordine dei dati
   console.log("Data after sorting:", dataObj);
   
   // Calcola l'altezza totale necessaria per visualizzare tutti i glifi
   let totalWidth = waveWidth * dataObj.length + waveAmplitude * (175 + gutter); 
-  //aggiungo 100 perchè altrimenti l'ultimo rimane tagliato fuori dalla finestra
+  //aggiungo 175 perchè altrimenti l'ultimo rimane tagliato fuori dalla finestra
+  //let totalHeight = 
   createCanvas(totalWidth, windowHeight);
   background("#c8ddd2");
 
   elem=createElement("h1", "Rivers in the world");
   elem.position(30,0);
   
-
   let xPos = 0;  // Posizione iniziale per il primo glifo
   let maxWaveLength = 0;  // Variabile per tracciare l'onda più lunga
 
   for (let i = 0; i < dataObj.length; i++) {
   
-    //waveLength mappata alla lunghezza del fiume:
-    // La variabile waveLength è calcolata usando map() in base 
+    // WaveLength mappata alla lunghezza del fiume:
+    // la variabile waveLength è calcolata usando map() in base 
     // alla lunghezza del fiume, per rendere ogni glifo proporzionale 
     // alla lunghezza effettiva del fiume. Più lungo è il fiume, maggiore 
     // sarà waveLength, rendendo il glifo visivamente più lungo.
-    // Map la lunghezza del fiume per decidere la larghezza del glifo
     
       let item = dataObj[i];
       
-      // Assicurati che `item.length` sia un numero valido
+      // Assicurarsi che `item.length` sia un numero valido
       if (!isNaN(item.length)) {
         // Mappa la lunghezza del fiume per decidere la larghezza del glifo
         // Modifica i valori 0 e 10000 in base ai dati reali
@@ -93,7 +95,8 @@ function setup() {
         // Disegna gli altri elementi (cerchio, outflow, lunghezza, ecc.)
         // Incrementa la posizione verticale per il prossimo glifo
         
-        // inserisco un piccolo cerchio 
+        // inserisco un piccolo cerchio per rappresentare il continente
+        // per semplicità di visualizzazion ho considerato i 5 principali
         if(item.continent==="Africa"){
           fill("black");
         } else if(item.continent==="South America" || item.continent==="North America"){
@@ -105,8 +108,8 @@ function setup() {
         }else if(item.continent==="Oceania" || item.continent==="Australia"){
           fill("blue");
         }
-
-        noStroke();
+        stroke("black");
+        strokeWeight(0.5);
         circle(xPos+70, yPos-30, 10);
 
       // Posiziona il testo dell'outflow alla fine dell'onda in orizzontale
@@ -116,8 +119,6 @@ function setup() {
       textSize(10);
       text(item.outflow, xPos+80, waveLength+20); // Posiziona il testo orizzontale dopo la fine dell'onda
         
-     
-   
       textSize(12);
       text(item.length, xPos+70, yPos-10); 
 
@@ -131,13 +132,8 @@ function setup() {
       // legenda
     elem=createElement("h4","Legenda");
     elem.position(30, maxWaveLength + 35);
-
- 
-
-    // Disegna un'onda corta nella legenda
     drawShortWave(50, maxWaveLength + yPos -30);
   }
-
 
 function draw() {
   // Il disegno è statico, quindi non serve codice qui
@@ -149,9 +145,7 @@ function drawGlyph(xPos, waveLength) {
   stroke("#5980d4");
   noFill();
   
-  
-
-  // Disegna un'onda orizzontale con la larghezza proporzionale 
+  // Disegna un'onda verticalmente con la lunghezza proporzionale ai dati del csv
   // alla lunghezza del fiume trasposta utilizzando il comando map
 
     beginShape();
@@ -162,9 +156,12 @@ function drawGlyph(xPos, waveLength) {
     endShape();
 }
 
-
+// funzione per disegnare la legenda
 function drawShortWave(xPos, yPos) {
-  noStroke();
+ 
+  stroke("black");
+  fill("grey");
+  strokeWeight(0.5);
   circle(xPos+15, yPos-20, 10);
   
   push();
@@ -172,7 +169,7 @@ function drawShortWave(xPos, yPos) {
   noStroke();
   textAlign(CENTER); // Allinea il testo al centro
   textSize(10);
-  text('length', xPos+20, yPos-5); // Scrive il nome del fiume
+  text('length', xPos+20, yPos-5); // Scrive la lunghezza del fiume
   pop();
 
   push();
@@ -181,10 +178,10 @@ function drawShortWave(xPos, yPos) {
   translate(xPos +100, yPos+80); // Posiziona il testo alla base dell'onda
   rotate(-HALF_PI); // Ruota di 90 gradi 
   textAlign(CENTER); // Allinea il testo al centro
-  textSize(10);
-  text('river name', 60, -100); // Scrive il nome del fiume
+  textSize(11);
+  textStyle(BOLD);
+  text('river name', 55, -100); // Scrive il nome del fiume
   pop();
-
 
   stroke("#5980d4");
   strokeWeight(3);
@@ -201,5 +198,39 @@ function drawShortWave(xPos, yPos) {
   fill(0);
   noStroke();
   textSize(10);
-  text("Outflow", xPos+10, yPos + 60);
+  text("Outflow", xPos+20, yPos + 65);
+  
+  stroke("black");
+  strokeWeight(0.5);
+  line(xPos+60, yPos-30, xPos+60, yPos+55);
+
+  fill("black");
+  circle(xPos+85, yPos-20, 10);
+  elem=createElement("h5","  --> Africa"); 
+  // il continente Africa è rappresentato dal pallino nero
+  elem.position(xPos+95, yPos-51);
+
+  fill("red");
+  circle(xPos+85, yPos-5, 10);
+  elem=createElement("h5","  --> America");
+  // il continente America è rappresentato dal pallino rosso
+  elem.position(xPos+95, yPos-36);
+
+  fill("yellow");
+  circle(xPos+85, yPos+10, 10);
+  elem=createElement("h5","  --> Asia");
+  // il continente Asia è rappresentato dal pallino giallo
+  elem.position(xPos+95, yPos-21);
+
+  fill("green");
+  circle(xPos+85, yPos+25, 10);
+  elem=createElement("h5","  --> Europe");
+  // il continenteEuropa è rappresentato dal pallino verde
+  elem.position(xPos+95, yPos-6);
+
+  fill("blue");
+  circle(xPos+85, yPos+40, 10);
+  elem=createElement("h5","  --> Oceania");
+  // il continente Oceania è rappresentato dal pallino blu
+  elem.position(xPos+95, yPos+9);
 }
